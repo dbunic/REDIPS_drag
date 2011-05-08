038fc3380f49bbe9e4cf24cf346fafb91609c2c9
Darko Bunic <darko.bunic@gmail.com>
2011-05-08 11:43:51 +0200

    Adding example16 - AJAX, hover, sticky, dragging

---
+++ example16/get-content.php
@@ -0,0 +1,34 @@
+<?php 
+
+// test if input parameter is set
+if (isset($_GET['id'])){
+	$id = $_GET['id'];
+}
+// otherwise set 0 for input value
+else {
+	$id = 0;
+}
+
+// "Lorem ipsum" string
+$lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ' .
+				'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' .
+				'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ' .
+				'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla ' .
+				'pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia '.
+				'deserunt mollit anim id est laborum.';
+
+// calculate string length from DIV id (use only first two digits and multiply with 10)
+// this only for demo purpose to show text length variation in DIV boxes - don't think too much about it
+$len = substr(preg_replace('/[^\d]+/i', '', $id), 0, 2) * 10;
+
+// prepare substring from "Lorem ipsum" string
+$rnd_text = substr($lorem_ipsum, 0, strrpos(substr($lorem_ipsum, 0, $len), ' ')) . ' ...';
+
+?>
+<div class="content">
+<h3>Question - <?php print($id) ?></h3>
+<img src="flower.png" width="100"/>
+<br/>
+<br/>
+<?php print($rnd_text)?>
+</div>
---
+++ example16/script.js
@@ -0,0 +1,237 @@
+/*jslint white: true, browser: true, undef: true, nomen: true, eqeqeq: true, plusplus: false, bitwise: true, regexp: true, strict: true, newcap: true, immed: true, maxerr: 14 */
+/*global window: false,  ActiveXObject: false, REDIPS: true */
+
+/* enable strict mode */
+"use strict";
+
+
+var request,						// XMLHttp request object
+	hover_div,						// reference to the hover_div object
+	size = {w: 0, h: 0},			// size of DIV elements in question table
+	width = '240px',				// width of DIV element dropped to the right table
+	content_url = 'get-content.php',// get-content URL
+	// functions
+	initXMLHttpClient,				// create XMLHttp request object in a cross-browser manner
+	send_request,					// send request to the server and display response in obj.innerHTML
+	show_tooltip,					// show tooltip (when mouse is over element in question table)
+	hide_tooltip,					// hide tooltip
+	toggle,							// function shows/hides tables in right container
+	hide_tables,					// initially hide all tables but first table (also set pageBreakBefore style)
+	single_content,					// set class="single" to all cells in question table (left table)
+	set_events,						// set onmouseover & onmouseout to all div elements inside DIV id="drag"
+	tc;								// target container (defined in myhandler_dropped_before() and used in myhandler_dropped())
+	
+	
+// initialization
+window.onload = function () {
+	// reference to the REDIPS.drag class
+	var rd = REDIPS.drag;
+	// set reference to the hover_div
+	hover_div = document.getElementById('hover_div');
+	// initially hide all tables but first table
+	hide_tables();
+	// set class="single" to the table cells question table (left table)
+	single_content();
+	// set onmouseover & onmouseout to all div elements inside DIV id="drag"
+	set_events();
+	// create XMLHttp request object
+	request = initXMLHttpClient();
+	// set fixed position of the left container
+	document.getElementById('left').style.position = 'fixed';
+	// initialization
+	rd.init();
+	// set hover color
+	rd.hover_color = '#9BB3DA';
+	// define 'switch' drop option (content can be exchanged)
+	rd.drop_option = 'switch';
+	// in a moment when dragging starts, remove mouseover event and hide hover tooltip
+	rd.myhandler_moved = function () {
+		REDIPS.event.remove(rd.obj, 'mouseover', show_tooltip);
+		hide_tooltip();
+	};
+	// event handler called before DIV element is dropped to the table
+	// in case when DIV element changes location from left to right DIV container or vice versa 
+	rd.myhandler_dropped_before = function (target_cell) {
+		var id = rd.obj.id,		// define id of DIV element
+			sc = rd.source_cell;// define start point for source container
+		// define start point of target container (global variable)
+		tc = target_cell;
+		// loop up until found target DIV container 
+		while (tc && tc.nodeName !== 'DIV') {
+			tc = tc.parentNode;
+		}
+		// loop up until found source DIV container
+		while (sc && sc.nodeName !== 'DIV') {
+			sc = sc.parentNode;
+		}
+		// if element is dropped from question table to the one of right tables
+		// (right tables doesn't have id)
+		if (sc.id === 'left' && tc.id === 'right') {
+			// send request (input parameter is object reference)
+			send_request(rd.obj, id);
+			rd.obj.style.width = width; // width parameter is set as global variable
+			rd.obj.style.height = '';
+		}
+		// if element is dropped from right table to the question table
+		else if (sc.id === 'right' && tc.id === 'left') {
+			rd.obj.innerHTML = id;
+			rd.obj.style.width = size.w;
+			rd.obj.style.height = size.h;
+		}
+	};
+	// after DIV element is dropped, return mouseover event (needed for tooltip in left table)
+	rd.myhandler_dropped = function (target_cell) {
+		// target container is defined in myhandler_dropped_before()
+		if (tc.id === 'left') {
+			REDIPS.event.add(rd.obj, 'mouseover', show_tooltip);
+		}
+	};
+};
+
+
+// XMLHttp request object
+initXMLHttpClient = function () {
+	var XMLHTTP_IDS,
+		xmlhttp,
+		success = false,
+		i;
+	// Mozilla/Chrome/Safari/IE7/IE8 (normal browsers)
+	try {
+		xmlhttp = new XMLHttpRequest(); 
+	}
+	// IE (?!)
+	catch (e1) {
+		XMLHTTP_IDS = [ 'MSXML2.XMLHTTP.5.0', 'MSXML2.XMLHTTP.4.0',
+						'MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLHTTP' ];
+		for (i = 0; i < XMLHTTP_IDS.length && !success; i++) {
+			try {
+				success = true;
+				xmlhttp = new ActiveXObject(XMLHTTP_IDS[i]);
+			}
+			catch (e2) {}
+		}
+		if (!success) {
+			throw new Error('Unable to create XMLHttpRequest!');
+		}
+	}
+	return xmlhttp;
+};
+
+
+// send request to the server and display response in obj.innerHTML
+send_request = function (obj, id) {
+	// open asynchronus request
+	request.open('GET', content_url + '?id=' + id, true);
+	// the onreadystatechange event is triggered every time the readyState changes
+	request.onreadystatechange = function () {
+		//  request finished and response is ready
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				obj.innerHTML = request.responseText;
+			}
+			// if request status isn't OK
+			else {
+				obj.innerHTML = 'Error: [' + request.status + '] ' + request.statusText;
+			}
+	    }
+	};
+	request.send(null); // send request
+};
+
+
+// show tooltip (when mouse is over element of question table) 
+show_tooltip = function (e) {
+	var element = e.target || e.srcElement,	// define element from the fired event
+		id = element.id,					// id of the DIV element is question ID (written as inner HTML)
+		box = element,						// remember box object (needed for offset calculation)
+		scrollPosition,						// scroll position
+		oTop, oLeft;						// offset Top & offset Left
+	// get scroll position and include scroll position in offset calculation
+	// use REDIPS.drag.getScrollPosition() for scroll position
+	scrollPosition = REDIPS.drag.getScrollPosition();
+	oLeft = scrollPosition[0];
+	oTop = scrollPosition[1];
+	// loop to the root element and return box offset (top, right, bottom, left)
+	do {
+		oLeft += box.offsetLeft;
+		oTop += box.offsetTop;
+		box = box.offsetParent;
+	}
+	while (box && box.nodeName !== 'BODY');
+	// set popup near to the element
+	hover_div.style.top  = (oTop + 22) + 'px';
+	hover_div.style.left = oLeft + 'px';
+	// send request (input parameter is object reference)
+	send_request(hover_div, id);
+	// set visibility
+	hover_div.style.visibility = 'visible';
+};
+
+
+// hide tooltip
+hide_tooltip = function () {
+	hover_div.style.visibility = 'hidden';	
+};
+
+
+// function shows/hides tables in right container
+// input parameters are button reference (to change button label) and id of table to show/hide
+toggle = function (btn, tbl_id) {
+	var tbl = document.getElementById(tbl_id);
+	if (tbl.style.display === '') {
+		btn.value = 'Table';
+		tbl.style.display = 'none';
+	}
+	else {
+		btn.value = 'Hide';
+		tbl.style.display = '';
+	}
+};
+
+
+// set onmouseover & onmouseout to all div elements inside DIV id="drag"
+set_events = function () {
+	var regex_drag = /\bdrag\b/i,	// regular expression to search "drag" class name
+		div, i;
+	// collect all div elements inside DIV id="drag"
+	div = document.getElementById('drag').getElementsByTagName('div');
+	for (i = 0; i < div.length; i++) {
+		// only DIV elements that contains "drag" in class name
+		if (regex_drag.test(div[i].className)) {
+			REDIPS.event.add(div[i], 'mouseover', show_tooltip);
+			REDIPS.event.add(div[i], 'mouseout', hide_tooltip);
+		}
+	}
+	// remember size (width and height) of DIV elements in question table
+	// needed in case when element is returned to the left table
+	size.w = div[0].style.width;
+	size.h = div[0].style.height;
+};
+
+
+// initially hide all tables but first table
+// hide them and set pageBreakBefore to 'always'
+// every table should be printed on separate paper
+hide_tables = function () {
+	var tbl, i;
+	// collect tables from right DIV container
+	tbl = document.getElementById('right').getElementsByTagName('table');
+	// hide all tables but first and set pageBreakBefore style
+	for (i = 1; i < tbl.length; i++) {
+		tbl[i].style.display = 'none';
+		tbl[i].style.pageBreakBefore = 'always';
+	}
+};
+
+
+// set class="single" to all cells in question table (left table)
+single_content = function () {
+	var cell, i;
+	// collect table cells from left table
+	cell = document.getElementById('table1').getElementsByTagName('td');
+	// set class='single' to all table cells
+	// to prevent 'switching' content from left and right tables
+	for (i = 0; i < cell.length; i++) {
+		cell[i].className = 'single';
+	}
+};
