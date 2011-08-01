ec3a648704512ebcb66a4ad09e42de1b6e61c2b9
Darko Bunic <darko.bunic@gmail.com>
2011-08-01 12:28:46 +0200

    Fixing save.php Problems with deleting the last element

--- example03/save.php
+++ example03/save.php
@@ -8,24 +8,26 @@ sqlQuery('start transaction');
 // delete all
 sqlQuery('delete from redips_timetable');
 
-// accept parameters (p is array)
-$arr = $_REQUEST['p'];
+// accept parameters - p is array (suppress errors by adding a @ sign)
+$arr = @$_REQUEST['p'];
 
-// open loop through each array element
-foreach ($arr as $p) {
-	// detach values from combined parameters
-	// $tbl parameter is ignored because saving goes only from table 1
-	list($sub_id, $tbl, $row, $col) = explode('_', $p);
-	// discard clone id part from the sub_id
-	$sub_id = substr($sub_id, 0, 2);
-	// insert to the database
-	sqlQuery("insert into redips_timetable (sub_id, tbl_row, tbl_col) values ('$sub_id', $row, $col)");
+// if input array exists (in all cases except deleting last element)
+if (is_array($arr)) {
+	// open loop through each array element
+	foreach ($arr as $p) {
+		// detach values from combined parameters
+		// $tbl parameter is ignored because saving goes only from table 1
+		list($sub_id, $tbl, $row, $col) = explode('_', $p);
+		// discard clone id part from the sub_id
+		$sub_id = substr($sub_id, 0, 2);
+		// insert to the database
+		sqlQuery("insert into redips_timetable (sub_id, tbl_row, tbl_col) values ('$sub_id', $row, $col)");
+	}
 }
 
 // commit transaction (sqlCommit is function from config.php)
 sqlCommit();
 
-// make redirect to the index.php
+// redirection to the index.php
 header('location: index.php');
-
 ?>
