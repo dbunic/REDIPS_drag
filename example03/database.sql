e811576e1f6bda65b76b6e8f4e4c6bfefdf5d949
dbunic <darko.bunic@gmail.com>
2012-03-06 08:26:39 +0100

    Define table key for redips_subject table

--- example03/database.sql
+++ example03/database.sql
@@ -6,17 +6,32 @@ Feb, 2010.
 
 */
 
+/* create timetable */
 create table redips_timetable (
-  tbl_id int(10) unsigned not null auto_increment,
-  sub_id char(2) not null,
-  tbl_row smallint(6) not null,
-  tbl_col smallint(6) not null,
-  primary key (`tbl_id`) using btree
+    tbl_id int(10) unsigned not null auto_increment,
+    sub_id char(2) not null,
+    tbl_row smallint(6) not null,
+    tbl_col smallint(6) not null,
+    primary key (`tbl_id`) using btree
 );
 
+
+/* create subjects */
 create table redips_subject (
-  sub_id char(2) not null,
-  sub_name varchar(40) not null
+    sub_id char(2) not null,
+    sub_name varchar(40) not null,
+    primary key (`sub_id`) using btree
 );
 
-insert into redips_subject VALUES ('en','English'),('ma','Mathematics'),('ph','Physics'),('bi','Biology'),('ch','Chemistry'),('it','IT'),('ar','Arts'),('hi','History'),('et','Ethics');
+
+/* fill subjects */
+insert into redips_subject values
+('en','English'),
+('ma','Mathematics'),
+('ph','Physics'),
+('bi','Biology'),
+('ch','Chemistry'),
+('it','IT'),
+('ar','Arts'),
+('hi','History'),
+('et','Ethics');
